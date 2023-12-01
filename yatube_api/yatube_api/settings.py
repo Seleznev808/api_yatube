be5d3e506362c1a224a8a88164e5afd997790892
Vasily Seleznev <seleznev.2016@yandex.ru>
2023-12-02 00:23:46 +0300

    Update settings.py

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -1,6 +1,11 @@
+import os
+from datetime import timedelta
 from pathlib import Path
 
-from datetime import timedelta
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
